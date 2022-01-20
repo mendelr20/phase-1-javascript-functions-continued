173888f037df8776d58466dfa0ca2a6790e17f2b
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-01-20 12:26:53 -0500

    commit tree a21bd4e4d3bd169ab36615ece9ef498d07977929

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = `roller-skate`){
+   return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(job = `go to the office`){
+    return `This Monday, I will ${job}.`
+}
+
+const wrapAdjective = (function(sign){
+    return function highlight1(string1) {
+        return `You are ${sign}${string1}${sign}!`     
+    } 
+})
+    
